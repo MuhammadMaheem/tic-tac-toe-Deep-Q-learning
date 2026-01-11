aa1a26cc9806b110b87b56df60c23eff91f52b5d
chsarah708 <chsarah708@gmail.com>
2026-01-11 23:12:35 +0500

    Render Ready

--- server.py
+++ server.py
@@ -207,4 +207,5 @@ if __name__ == '__main__':
     print('  👉 http://localhost:8000')
     print('='*50)
     
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
